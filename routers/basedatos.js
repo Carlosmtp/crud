8af1ba1cdb811f0b055bb344697b518bb613ece6
Carlos Mauricio Tovar Parra <carlos.mauricio.tovar@correounivalle.edu.co>
2020-09-21 18:43:52 -0500

    basado en la plantilla del profesor, se modifico el archivo basedatos.js

--- routers/basedatos.js
+++ routers/basedatos.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 const Router = require('express-promise-router');
 
 const pool = new Pool({
-  user: 'fredyb',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'univalle',
+  user: 'uhxqnbth',
+  host: 'lallah.db.elephantsql.com',
+  database: 'uhxqnbth',
+  password: 'Udz8WastnUAH32fdJUMShysHpr6-ps0J',
   port: 5432,
 });
 
@@ -13,16 +13,28 @@ const router = new Router();
 // export our router to be mounted by the parent application
 module.exports = router;
 
+router.post('/insertarpacientes', async (req, res) => {
+  const { id, nombre, apellido } = req.body;
+  await pool.query(
+    `INSERT INTO pacientes(id, nombre, apellido) VALUES('${id}','${nombre}','${apellido}')`
+  );
+  res.send('INSERTADO');
+});
+
 router.get('/consultatotalpacientes', async (req, res) => {
   //const { id } = req.params
   const { rows } = await pool.query('SELECT * FROM pacientes');
   res.send(rows);
 });
 
-router.post('/insertarpacientes', async (req, res) => {
-  const { nombre, apellido, numid } = req.body;
-  await pool.query(
-    `INSERT INTO pacientes(nombre, apellido, numid) VALUES('${nombre}','${apellido}','${numid}')`
-  );
-  res.send('INSERTADO');
+router.put('/editarpaciente', async (req, res) => {
+  const {id, nombre, apellido} = req.body;
+  await pool.query('UPDATE pacientes SET nombre = $1, apellido = $2 WHERE id= $3', [nombre, apellido, id]);
+  res.send('ACTUALIZADO');
+});
+
+router.delete('/eliminarpaciente', async(req, res) => {
+  const {id} = req.body;
+  await pool.query('DELETE FROM pacientes WHERE id = $1',[id]);
+  res.send('ELIMINADO');
 });
